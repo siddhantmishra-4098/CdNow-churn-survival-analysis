557df5557343e8de81d4b1fe8cf812ab28166635
Siddhant Mishra <90679433+siddhantmishra-4098@users.noreply.github.com>
2026-05-16 16:36:39 +0200

    Delete Cdnow directory

--- Cdnow/tests/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-with base as (
-	select
-	DATEDIFF (month,min(date) over(partition by customer_id),max(date) over(partition by customer_id)) as durations
-	from bronze.cdNow
-	)
-
-select * from base 
-where durations<6
--- Cdnow/tests/gold.sql
+++
@@ -1,6 +0,0 @@
-exec gold.load_gold
-
-select * from GOLD.cdnow
-order by customer_id
-
-select * from silver.CdNow
